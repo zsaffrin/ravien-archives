5367c5698390f6199629a7374b6645bbe386c539
Zach Saffrin <zach@zachsaffrin.com>
2019-06-04 21:47:56 -0400

    Layout cleanup

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,23 +1,65 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const sortIntoCategories = items =>
+  items.reduce((acc, item) => {
+    const category = item.frontmatter.category
+    if (!(category in acc)) {
+      acc[category] = []
+    }
+    acc[category].push(item)
+    return acc
+  }, {})
+
 const ArticlesPage = ({ data }) => {
+  const articles = sortIntoCategories(data.articles.nodes)
+
+  const PageLayout = styled.div(({ theme }) => {
+    const { space } = theme
+    return `
+      display: grid;
+      grid-gap: ${space.xl};
+      padding: ${space.xl};
+    `
+  })
+  const Section = styled.div(({ theme }) => {
+    const { space } = theme
+    return `
+      display: grid;
+      grid-gap: ${space.lg};
+    `
+  })
+  const CategoryHeader = styled.h2`
+    margin: 0;
+    text-transform: capitalize;
+  `
+
   return (
     <Layout>
       <SEO title="Articles" />
-      <div>
-        {data.articles.nodes.map(({ fields, frontmatter }) => {
-          const { title } = frontmatter
-          return (
-            <div key={title}>
-              <Link to={fields.slug}>{title}</Link>
+      <PageLayout>
+        <h1>Articles</h1>
+        {Object.keys(articles).map(key => (
+          <Section key={key}>
+            <CategoryHeader>{key}</CategoryHeader>
+            <div>
+              {articles[key].map(article => {
+                const { slug } = article.fields
+                const { title } = article.frontmatter
+                return (
+                  <div key={title}>
+                    <Link to={slug}>{title}</Link>
+                  </div>
+                )
+              })}
             </div>
-          )
-        })}
-      </div>
+          </Section>
+        ))}
+      </PageLayout>
     </Layout>
   )
 }
--- src/templates/category.js
+++ src/templates/category.js
@@ -1,23 +1,40 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Category = ({ data }) => {
+const Category = ({ data, pageContext }) => {
+  const PageLayout = styled.div(({ theme }) => {
+    const { space } = theme
+    return `
+      display: grid;
+      grid-gap: ${space.xl};
+      padding: ${space.xl};
+    `
+  })
+  const PageTitle = styled.h1`
+    margin: 0;
+    text-transform: capitalize;
+  `
+
   return (
     <Layout>
       <SEO title="Category" />
-      <div>
-        {data.articles.nodes.map(({ fields, frontmatter }) => {
-          const { title } = frontmatter
-          return (
-            <div key={title}>
-              <Link to={fields.slug}>{title}</Link>
-            </div>
-          )
-        })}
-      </div>
+      <PageLayout>
+        <PageTitle>{pageContext.category}</PageTitle>
+        <div>
+          {data.articles.nodes.map(({ fields, frontmatter }) => {
+            const { title } = frontmatter
+            return (
+              <div key={title}>
+                <Link to={fields.slug}>{title}</Link>
+              </div>
+            )
+          })}
+        </div>
+      </PageLayout>
     </Layout>
   )
 }
