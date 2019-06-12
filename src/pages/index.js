9b44369ae295d1f0c98d84a09d653792aba53f1f
Zach Saffrin <zach@zachsaffrin.com>
2019-06-11 22:05:56 -0400

    Mark progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,29 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    Stuff to make
-    <ul>
-      <li>Player Characters</li>
-      <li>Locations</li>
-      <li>Lore / background</li>
-      <li>People / groups</li>
-      <li>Missions / quests</li>
-      <li>Session log</li>
-      <li>Loot claimer</li>
-      <li>Map</li>
-    </ul>
-  </Layout>
-)
+const IndexPage = () => {
+  const Struck = styled.li`
+    text-decoration: line-through;
+  `
+  return (
+    <Layout>
+      <SEO title="Home" />
+      Stuff to make
+      <ul>
+        <Struck>Player Characters</Struck>
+        <Struck>Locations</Struck>
+        <li>Lore / background</li>
+        <Struck>People / groups</Struck>
+        <li>Missions / quests</li>
+        <li>Session log</li>
+        <li>Loot claimer</li>
+        <li>Map</li>
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
